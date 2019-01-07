018b880ef210d4473a851efcd0eacf397f744d4d
Yoann Frommelt <yfrommelt@hevaweb.com>
2019-01-07 16:30:49 +0100

    add toTitleCase and toSlug
    add `les` in french stopwords

--- src/StopWords.php
+++ src/StopWords.php
@@ -4,6 +4,11 @@ namespace Yfrommelt;
 
 class StopWords
 {
+    /**
+     * @param string $locale
+     * @return array
+     * @throws \Exception
+     */
     public static function get($locale = 'en')
     {
         $locales = [
@@ -31,5 +36,65 @@ class StopWords
             sprintf('%s/dictionary/%s', __DIR__, $locales[$locale]),
             FILE_IGNORE_NEW_LINES
         );
+    }
+
+    /**
+     * @param string $locale
+     * @param string $str
+     * @param string $encoding
+     * @return string
+     * @throws \Exception
+     */
+    public static function toTitleCase($locale, $str, $encoding = 'UTF-8')
+    {
+        $stopWords = self::get($locale);
+        $delimiters = [' ', '.', '-'];
+
+        $str = mb_convert_case($str, MB_CASE_TITLE, $encoding);
+
+        foreach ($delimiters as $delimiter) {
+            $words = explode($delimiter, $str);
+            $keeps = [];
+            foreach ($words as $word) {
+                $lower = mb_strtolower($word, $encoding);
+                if (in_array($lower, $stopWords)) {
+                    $word = $lower;
+                } else {
+                    $word = ucfirst($word);
+                }
+                array_push($keeps, $word);
+            }
+            $str = join($delimiter, $keeps);
+        }
+
+        return $str;
+    }
+
+    /**
+     * @param string $locale
+     * @param string $str
+     * @param string $encoding
+     * @return string
+     * @throws \Exception
+     */
+    public static function toSlug($locale, $str, $encoding = 'UTF-8')
+    {
+        $stopWords = self::get($locale);
+        $delimiters = [' ', '.', '-'];
+
+        $str = mb_strtolower($str, $encoding);
+
+        foreach ($delimiters as $delimiter) {
+            $words = explode($delimiter, $str);
+            $keeps = [];
+            foreach ($words as $word) {
+                if (!in_array($word, $stopWords)) {
+                    array_push($keeps, $word);
+                }
+            }
+            $str = join('-', $keeps);
+        }
+
+        return $str;
     }
 }
